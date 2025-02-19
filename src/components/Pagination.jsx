bbd315f0b86a60a1adc1c5e25342686bb2fe664d
Michelle <jiahu0724417@gmail.com>
2025-02-19 09:28:04 +0800

    edit a link to button

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,16 +4,16 @@ const Pagination = ({ pageData, handlePageChange }) => {
       <div className='d-flex justify-content-center mt-5'>
           <nav aria-label="Page navigation">
               <ul className="pagination">
-                  <li className={`page-item ${!pageData.has_pre && 'disabled'}`}><a onClick={() => handlePageChange(pageData.current_page-1)} className="page-link" href="#">上一頁</a>
+                  <li className={`page-item ${!pageData.has_pre && 'disabled'}`}><button type="button" onClick={() => handlePageChange(pageData.current_page-1)} className="page-link">上一頁</button>
                   </li>
   
                   {Array.from({ length: pageData.total_pages }).map((_, index) => (
                       <li key={index} className={`page-item ${pageData.current_page === index +1 && 'active'}`}>
-                          <a onClick={() => handlePageChange(index+1)} className="page-link" href="#">{index+1}</a>
+                          <button type="button" onClick={() => handlePageChange(index+1)} className="page-link">{index+1}</button>
                       </li>
                   ))}
   
-                  <li className={`page-item ${!pageData.has_next && 'disabled'}`}><a onClick={() => handlePageChange(pageData.current_page+1)} className="page-link" href="#">下一頁</a>
+                  <li className={`page-item ${!pageData.has_next && 'disabled'}`}><button type="button" onClick={() => handlePageChange(pageData.current_page+1)} className="page-link">下一頁</button>
                   </li>
               </ul>
           </nav>
